ec3e17652de321acfd55017850d2e5e3fd460a5e
Naisarg-Patel <23012011066@gmail.com>
2026-02-05 21:07:16 +0530

    new

--- js/api.js
+++ js/api.js
@@ -4,7 +4,8 @@ async function loginUser(email, password) {
   const res = await fetch(`${API_URL}/login`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ email, password }),
+    body: JSON.stringify({ email : email,
+                           password : password }),
   });
   return res.json();
 }
