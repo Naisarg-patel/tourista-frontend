b018c7dd9e0b4c4e2d2b0d1149db5a5b1ae2e212
Naisarg-Patel <23012011066@gmail.com>
2026-02-05 15:25:50 +0530

    Fix CORS issue

--- js/api.js
+++ js/api.js
@@ -1,19 +1,19 @@
 const API_URL = "https://tourista-backend-lyy0.onrender.com/api/auth";
 
-async function loginUser(data) {
+async function loginUser(email, password) {
   const res = await fetch(`${API_URL}/login`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(data),
+    body: JSON.stringify({ email, password }),
   });
   return res.json();
 }
 
-async function registerUser(data) {
+async function registerUser(name, email, password) {
   const res = await fetch(`${API_URL}/register`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(data),
+    body: JSON.stringify({ name, email, password }),
   });
   return res.json();
 }
